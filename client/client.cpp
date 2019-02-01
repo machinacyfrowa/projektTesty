a011d76ac5c35f62730b28763f8c96e139a2e1ee
Paweł Zawadzki <admin@machinacyfrowa.pl>
2019-02-01 12:21:39 +0100

    output added

--- client/client.cpp
+++ client/client.cpp
@@ -5,13 +5,39 @@
 #include <fstream>
 using namespace std;
 
+int testFlag = 0;
+int verboseFlag = 0;
+
+void v(string s) {
+	if(verboseFlag) fprintf(stdout, "%s\n", s.c_str());
+}
+void v(string s1, char* s2) {
+	if(verboseFlag) fprintf(stdout, "%s %s\n", s1.c_str(), s2);
+}
+void executeTestCommand(string line) {
+	switch(line.at(0)) {
+		case 'c':
+		v("Połączenie z xxx.");
+		break;
+		case 's':
+		v("Wysyłanie xxx.");
+		break;
+		case 'q':
+		v("Opuszczanie programu.");
+		break;
+		default:
+		v("Nieprawidłowe polecenie w pliku tekstowym.");
+		break;
+	}
+}
+
 int main (int argc, char **argv)
 {
-	int testFlag = 0;
+
 	char *testFileName = NULL;
 	char *outputFileName = NULL;
-	int verboseFlag = 0;
-	int index, c;
+	
+	int index, c, r;
 
 	opterr = 0;
 
@@ -31,29 +57,35 @@ int main (int argc, char **argv)
 			case '?':
 			if (optopt == 't')
           		fprintf (stderr, "Opcja -%c wymaga argumentu.\n", optopt);
-        	else fprintf(stderr, "Nieznana opcja: -%c.\n", optopt);
+        		else fprintf(stderr, "Nieznana opcja: -%c.\n", optopt);
 			return 1;
 			break;
 			default:
 			abort();
 		}
 	if(verboseFlag == 1){
-		fprintf(stdout, "Ustawiono flagę gadatliwą.\n");
-		if(testFlag == 1) fprintf(stdout, "Ustawiono flagę testową, plik z poleceniami: %s.\n", testFileName);
-		if(outputFileName != NULL) fprintf(stdout, "Ustawiono plik wyjścia dla testów na: %s.\n", outputFileName);
+		v("Ustawiono flagę \"gadatliwą\".");
+		if(testFlag == 1) v("Ustawiono flagę testową");
+		if(testFileName != NULL) v("Ustawiono plik z poleceniami testowymi", testFileName);
 	}
 	if(testFlag == 1) {
 		//run automated tests based on file contents
 		//open file first
-		ifstream file(testFileName);
-		if (file.is_open()) {
+		ifstream inputFile(testFileName);
+		ofstream outputFile(outputFileName);
+		if (inputFile.is_open()) {
 			string line;
-    		while (getline(file, line)) {
-        	// using printf() in all tests for consistency
-       		fprintf(stdout, "%s\n", line.c_str());
+			srand (time(NULL));
+    			while (getline(inputFile, line)) {
+				executeTestCommand(line);
+				r = rand() % 10 + 1;
+				if(r % 2 == 0) outputFile << "PASS" << endl;
+				else outputFile << "FAIL" << endl;
 			}
-	 		file.close();
+	 		inputFile.close();
+			outputFile.close();
 		}
 	}
-	fprintf(stdout, "Koniec\n");
+	v("Koniec");
+	return 0;
 }
